ee988238c045bbdb0b96dd5297c02e51cede693b
Glenn-Jaya <jayasugp@myumanitoba.ca>
2021-02-19 19:51:38 -0600

    cleaned processes and threads header files

--- processes.h
+++ processes.h
@@ -2,15 +2,12 @@
 #define PROC_H
 
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 
 #include "manager.h"
 
 void childWork(idManager processManager, int* work);
 void endChild(pid_t pid);
-
 void runProcesses(int totalProcesses, int maxConcurrent);
 
 #endif //PROC_H
